0c79acb4cdba6b95581bd5bec17bc37cad1faa19
Lovish Bains <2019ucs0093@iitjammu.ac.in>
2022-02-14 16:09:27 +0530

    decode

--- affinecipher.py
+++ affinecipher.py
@@ -1,34 +1,68 @@
 # this is the code for encryption of affine cipher
 
-print("please enter the plaintext")
-m = str(input())
 
-import random
-a = random.randint(1,26)
-b = random.randint(1,26)
-#these are the keys that we will be using for the encryption
-
-c = ""
-
-for i in m:
-    asci_value = ord(i)
-    #if i is a space we will ignore it
-    if(asci_value==32):
-        cip_let= asci_value
+def encode_affine(m,a,b):
+    c = ""
+    for i in m:
+        asci_value = ord(i)
+        #if i is a space we will ignore it
+        if(asci_value==32):
+            cip_let= asci_value
     
-    #IF i is hella small alphabet it lies between 97 and 122
-    elif(asci_value>=97 and asci_value<=122):
-        asci_value = asci_value-97
-        asci_value = a*(asci_value)+b
-        asci_value = asci_value%26
-        cip_let = asci_value+97
+        #IF i is hella small alphabet it lies between 97 and 122
+        elif(asci_value>=97 and asci_value<=122):
+            asci_value = asci_value-97
+            asci_value = a*(asci_value)+b
+            asci_value = asci_value%26
+            cip_let = asci_value+97
+        # for capital alphabets
+        elif(asci_value>=65 and asci_value<=90):
+            asci_value = asci_value-65
+            asci_value = a*(asci_value)+b
+            asci_value = asci_value%26
+            cip_let = asci_value+65     
 
-    elif(asci_value>=65 and asci_value<=90):
-        asci_value = asci_value-65
-        asci_value = a*(asci_value)+b
-        asci_value = asci_value%26
-        cip_let = asci_value+65     
+        c = c+ chr(cip_let)
+    return c
 
-    c = c+ chr(cip_let)
+print("please select Encode(1) or decode(0)")
+i = int(input())
 
-print("the ciphertext is ", c, "and the key is (",a,",",b ,").")
+if(i==1):
+    print("Please enter the plaintext")
+    m = str(input())
+    import random
+    a = random.randint(1,26)
+    b = random.randint(1,26)
+    c = encode_affine(m,a,b)
+    print("the ciphertext is ", c, "and the key is (",a,",",b ,").")
+
+def decode_affine(c):
+    for a in range(1,27):
+        for b in range(1,27):
+            m = ""
+            for i in c:
+                asci_val = ord(i)
+                if(asci_val==32):
+                    dec_val = i
+                
+                elif(asci_val>=97 and asci_val<=122):
+                    asci_val = asci_val-97
+                    dec_val = ((asci_val-b)/a)%26
+                    dec_val = dec_val +97
+                
+                elif(asci_val>=65 and asci_val<=90):
+                    asci_val = asci_val-65
+                    dec_val = ((asci_val-b)//a)%26
+                    print(dec_val)
+                    dec_val = dec_val +65  
+                m = m+chr(dec_val)
+            
+            print(m,"\n")
+
+if(i==0):
+    print("please enter the ciphertext")
+    c = str(input())
+    decode_affine(c)
+    print("select the meaningful sentence")
+
--- shiftcipher.py
+++ shiftcipher.py
@@ -1,41 +1,75 @@
 #this is the code for shift cipher
 
-print("please enter the plaintext")
+#print("please enter the plaintext")
+#m = str(input())
+
+print("please enter the ciphertext")
 m = str(input())
 
-import random
+#import random
 #k = random.randint(1,26)
-k = 18
+
 # this will act as our key
+def encode_shift(m,k):
+    c = ""
+    # this is our cipher text we will traverse through the string and create our final cipher text
+    for i in m:
+        asci_value = ord(i)
+        #if i is a space we will ignore it
+        if(asci_value==32):
+            cip_let= asci_value
 
-c = ""
-# this is our cipher text we will traverse through the string and create our final cipher text
-
-for i in m:
-    asci_value = ord(i)
-    #if i is a space we will ignore it
-    if(asci_value==32):
-        cip_let= asci_value
+        #IF i is a small alphabet it lies between 97 and 122
+        elif(asci_value>=97 and asci_value<=122):
+            asci_value = asci_value-97
+            cip_let = asci_value+k
+            cip_let = cip_let%26
+            cip_let = cip_let+97
     
-    #IF i is a small alphabet it lies between 97 and 122
-    elif(asci_value>=97 and asci_value<=122):
-        asci_value = asci_value-97
-        cip_let = asci_value+k
-        cip_let = cip_let%26
-        cip_let = cip_let+97
-   
-    #if i is a capital letter it lies between 65 and 90
-    elif(asci_value>=65 and asci_value<=90):
-        asci_value = asci_value-65
-        cip_let = asci_value+k
-        cip_let = cip_let%26
-        cip_let = cip_let+65
+        #if i is a capital letter it lies between 65 and 90
+        elif(asci_value>=65 and asci_value<=90):
+            asci_value = asci_value-65
+            cip_let = asci_value+k
+            cip_let = cip_let%26
+            cip_let = cip_let+65
 
-    c = c+chr(cip_let)
+        c = c+chr(cip_let)
+    return c
 
-print("the cipher text is", c, "and the key is", k)
+#c = encode_shift(m,k)
+#print("the cipher text is", c, "and the key is", k)
 
+def decode_shift_help(m,k):
+    c = ""
+    # this is our cipher text we will traverse through the string and create our final cipher text
+    for i in m:
+        asci_value = ord(i)
+        #if i is a space we will ignore it
+        if(asci_value==32):
+            cip_let= asci_value
 
+        #IF i is a small alphabet it lies between 97 and 122
+        elif(asci_value>=97 and asci_value<=122):
+            asci_value = asci_value-97
+            cip_let = asci_value-k
+            cip_let = cip_let%26
+            cip_let = cip_let+97
+    
+        #if i is a capital letter it lies between 65 and 90
+        elif(asci_value>=65 and asci_value<=90):
+            asci_value = asci_value-65
+            cip_let = asci_value-k
+            cip_let = cip_let%26
+            cip_let = cip_let+65
 
+        c = c+chr(cip_let)
+    return c
 
+def decode_shift(m):
+    for i in range(1,26):
+        c = decode_shift_help(m,i)
+        print(c," ", i, "\n")
+
+decode_shift(m)
+print("please choose the required key")
 
