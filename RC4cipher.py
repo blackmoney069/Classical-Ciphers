ccb1ac3d50377fb9b13643d0ccb810a827ebba8f
Lovish Bains <2019ucs0093@iitjammu.ac.in>
2022-03-02 16:29:02 +0530

    stream generation added

--- RC4cipher.py
+++ RC4cipher.py
@@ -1,20 +1,24 @@
 
-print('Please enter the size for the arrays')
-cip_len = int(input())
-S_array = [0]*cip_len
-K_array = [0]*cip_len
+print('Please enter the size of the S-array')
+S_len = int(input())
+S_array = [0]*S_len
+T_array = [0]*S_len
 
-for i in range(0,cip_len):
+for i in range(0,S_len):
     S_array[i] = i
 
-print("please enter the siez of the key")
+print("please enter the size of the key")
 key_len = int(input())
 
 print("please enter the key")
 key_str = str(input())
 
-for i in range(0,cip_len):
-    K_array[i] = int(x[i%key_len])
+K_array = [0]*key_len
+for i in range(0,key_len):
+    K_array[i] = int(key_str[i])
+
+for i in range(0,S_len):
+    T_array[i] = int(key_str[i%key_len])
 
 def swap_array(S, i,j):
     c = S[i]
@@ -29,12 +33,28 @@ def print_array(S_array):
 def key_scheduling_RC4(S,K):
     j=0
     for i in range(0,8):
-        j = (j+S[i]+K[i])%8
+        j = (j+S[i]+K[i])%S_len
         swap_array(S,i,j)
         print_array(S)
 
 
-key_scheduling_RC4(S_array,K_array)
+key_scheduling_RC4(S_array,T_array)
 
+def stream_generation(S,key_size):
+    i,j =0
+    outkey =[]
+    gen_size=0
+    while(gen_size <key_size):
+        i = (i+1)%S_len
+        j = (j+S[i])%S_len
+        swap_array(S,i,j)
+        t = (S[i]+S[j])%S_len
+        outkey.append(S[t])
+        gen_size+=1;
+    return outkey
+
+
+
+    
 
 
